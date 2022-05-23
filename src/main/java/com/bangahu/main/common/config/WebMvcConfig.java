822e346a74de5e192a79b15d65387107bc1c7e1b
sunjjoo <sunjjoo@gmail.com>
2022-05-24 01:21:08 +0900

    Eighth

--- src/main/java/com/bangahu/main/common/config/WebMvcConfig.java
+++ src/main/java/com/bangahu/main/common/config/WebMvcConfig.java
@@ -1,5 +1,36 @@
 package com.bangahu.main.common.config;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
 public class WebMvcConfig {
 
+	@Bean
+	public WebClient webClient() {
+		HttpClient httpClient = HttpClient.create()
+				  .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+				  .responseTimeout(Duration.ofMillis(5000))
+				  .doOnConnected(conn -> 
+				    conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+				      .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
+		
+		return WebClient.builder()
+				.clientConnector(new ReactorClientHttpConnector(httpClient))
+				.baseUrl("http://localhost:8090")
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				.build();
+	}
 }
--- src/main/java/com/bangahu/main/controller/TestController.java
+++ src/main/java/com/bangahu/main/controller/TestController.java
@@ -3,16 +3,42 @@ package com.bangahu.main.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.bangahu.main.model.domain.User;
 import com.bangahu.main.service.UserService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TestController {
 	private final UserService userService;
+	private final WebClient webClient;
+	
+	@GetMapping("/react/user")
+	public ResponseEntity<?> reactGetUser(String username) {
+		Mono<User> monoUser = webClient.get().uri(builder -> builder.path("/user").queryParam("username", username).build())
+			//.exchangeToMono(response -> response.bodyToMono(User.class)).log();
+			.exchangeToMono(response -> {
+				log.info("### response: {}-{}", response.statusCode(), response.bodyToMono(User.class));
+				return response.bodyToMono(User.class);
+			})
+			.log()
+			.onErrorResume(e -> {
+				log.debug("e: {}", e.getMessage());
+				return Mono.just(null);
+			});
+		
+//		Mono<User> monoUser = webClient.get().uri(builder -> builder.path("/user").queryParam("username", username).build())
+//				.retrieve().bodyToMono(User.class).log();
+		
+		//User user = userService.selectUser(username);
+		return ResponseEntity.ok(monoUser.block());
+	}
 	
 	@GetMapping("/user")
 	public ResponseEntity<?> getUser(String username) {
